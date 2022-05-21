da4ef52e9c89691f86026e8222e1d0d09da95d1a
Arshad <mohammedarshad183@gmail.com>
2022-05-21 23:17:02 +0530

    Test programs

--- TestPrograms/DelegatesReuse/Program.cs
+++ TestPrograms/DelegatesReuse/Program.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static DelegatesReuse.Program;
 
 namespace DelegatesReuse
 {
+    class DelegateProgram
+    {
+        public static void GetNumbers(int[] n,checkDel del)
+        {
+            foreach (int i in n)
+            {
+                if (del.Invoke(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
     class Program
     {
-        int[] data = { 1,10,32,40,50,70,80,50,20,60};
+        public delegate bool checkDel(int a); 
+        static int[] data = { 1, 10, 32, 40, 50, 70, 80, 50, 20, 60 };
         static void Main(string[] args)
         {
-            
+            checkDel d = check;
+            DelegateProgram.GetNumbers(data, d);
+            Console.ReadLine();
+        }
+
+
+        public static bool check(int a)
+        {
+            return a > 50;
         }
     }
 }
